66b5206086c8ac1d4781494636800f8154a51ce4
Probfia <46550166+Probfia@users.noreply.github.com>
2019-04-02 22:09:18 +0800

    Add files via upload

--- new_double_base.py
+++ new_double_base.py
@@ -3,22 +3,23 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
 #global parameters
-G = 1
-m1 = 10**-6
-m2 = 10**-3
-phipk = 4 #the location of the peak of the additional gaussian potential
-mu = 1
-alpha = 100
+Mpl = 1
+G = 1./(8*np.pi*Mpl**2)
+m1 = 10**-6*Mpl
+m2 = 10**-2*Mpl
+phipk = 4*Mpl #the location of the peak of the additional gaussian potential
+mu = 1*Mpl^2 # sigma^2 of the additional gaussian potential
+alpha = 5
 
 def V(phi,chi):
-    return 1./2*m1**2*phi**2*(1 + alpha*np.tanh(m2*chi)*np.exp(-(phi - phipk)**2/(2*mu)))
+    return 1./2*m1**2*phi**2*(1 + alpha*np.tanh(m2*chi/Mpl**2)*np.exp(-(phi - phipk)**2/(2*mu)))
 
 def V_phi(phi,chi):
-    eps = 10**-6
+    eps = 10**-6*Mpl
     return (V(phi+eps/2,chi) - V(phi-eps/2,chi))/eps
 
 def V_chi(phi,chi):
-    eps = 10**-6
+    eps = 10**-6*Mpl
     return (V(phi,chi+eps/2) - V(phi,chi-eps/2))/eps
 
 def d_dlna(var,lna,paras):
@@ -33,19 +34,20 @@ def d_dlna(var,lna,paras):
 def thirdH(phi,psi,chi,xi):
     return np.sqrt(2*V(phi,chi)/(3/(4*np.pi*G) - (psi**2 + xi**2)))
 
-phi0 = 6
+phi0 = 30
 psi0 = 0
-chi0 = 1
+chi0 = 4
 xi0 = 0
 H0 = thirdH(phi0,psi0,chi0,xi0)
 paras = [m1,m2]
 var0 = [H0,phi0,psi0,chi0,xi0]
 
-lnaStop = 200
+lnaStop = 300
 num_of_pts = 100000
 lnavals = np.linspace(0,lnaStop,num_of_pts)
 
 sol = odeint(d_dlna,var0,lnavals,args=(paras,))
+Hvals = sol[:,0]
 phivals = sol[:,1]
 psivals = sol[:,2]
 chivals = sol[:,3]
@@ -54,4 +56,10 @@ xivals = sol[:,4]
 plt.plot(lnavals,phivals)
 plt.plot(lnavals,chivals)
 
+plt.show()
+
+# check conservation of energy
+energy_vals = thirdH(phivals,psivals,chivals,xivals)**2 - (8*np.pi*G/3)*(0.5*Hvals**2*(psivals**2 + xivals**2) + V(phivals,chivals))
+
+plt.plot(lnavals,energy_vals)
 plt.show()
